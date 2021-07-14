b40ced039affd661ecae74b999c5e0448c4dba6f
Sean Spiegl <sean.spiegl@outlook.com>
2021-07-14 23:39:38 +0800

    fixed environment variables

--- src/js/globals.js
+++ src/js/globals.js
@@ -2,7 +2,7 @@ const isElectron = process.env.VUE_APP_ELECTRON === 'true';
 let platform = 'browser';
 if (isElectron) {
     platform = window.electron.platform;
-} 
+}
 
 const globals = Object.freeze({
     isDev: process.env.NODE_ENV === 'development',
